b9da0106b1ea6e7fd0626d8bb11b95876d57b20c
sample123@gmail.com <sample@gmail.com>
2026-03-20 12:01:37 +0530

    Practical - 1 Completed

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter 1st Integer : ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.Write("Enter 2nd Integer : ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+
+        int add = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        double divide = Convert.ToDouble(a) / Convert.ToDouble(b);
+
+        Console.WriteLine("Results : ");
+        Console.WriteLine();
+        Console.WriteLine($"Addition : {add}");
+        Console.WriteLine($"Subtraction : {sub}");
+        Console.WriteLine($"Multiplication : {mul}");
+        Console.WriteLine($"Division : {divide}");
+    }
+}
---
+++ DebuggingApplication/DebuggingApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter String : ");
+        string userResponse = Console.ReadLine();
+
+        if (Convert.ToBoolean(int.Parse(userResponse)))
+        {
+
+            Console.WriteLine("The entered value is a integer");
+
+        }
+    }
+}
